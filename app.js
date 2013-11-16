12c76061d72f66a364d7ba7adb57c3a120b01343
Aaron Tidwell <Aaron.Tidwell@gmail.com>
2013-11-15 22:17:54 -0500

    first

---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var staticDirectory = './dist';
+
+var nStatic = require('node-static'); //require
+var fileServer = new nStatic.Server(staticDirectory); //create
+
+//use native node http and proxy the request and response to
+//the node static instance
+require('http').createServer(function(request, response) {
+	request.addListener('end', function() {
+		// Serve files
+		fileServer.serve(request, response);
+	}).resume();
+}).listen(8080);
