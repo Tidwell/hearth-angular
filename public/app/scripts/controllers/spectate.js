a89531a0d3a51b3feca44197ced3927a2c33d7b5
Aaron Tidwell <Aaron.Tidwell@gmail.com>
2014-01-02 02:45:57 -0500

    working spectate with service

--- public/app/scripts/controllers/spectate.js
+++ public/app/scripts/controllers/spectate.js
@@ -1,25 +1,25 @@
 'use strict';
 
 angular.module('hearthApp')
-	.controller('SpectateCtrl', function($scope,$routeParams, socket) {
+	.controller('SpectateCtrl', function($scope,$routeParams, socket, tournaments) {
 		$scope.tournamentId = $routeParams.id;
+
+		$scope.t = tournaments.get();
 
 		function view() {
 			//$scope.tournamentName = tournament.tournament.name;
 			$('.view-iframe').challonge($scope.tournamentId, {subdomain: 'hs_tourney', theme: '2', multiplier: '1.0', match_width_multiplier: '1.0', show_final_results: '0', show_standings: '0'});
 		}
 
-		if ($scope.tournamentId) {
-			socket.emit('tournaments:info', $scope.tournamentId);
-		}
-
-		socket.on('tournaments:info', function(data){
-			if (data.error) {
-				$scope.error = data.error;
-				return;
-			}
-			$scope.activeTournament = data;
-			view();
+		$scope.$watch('t.tournaments', function(){
+			$scope.found = false;
+			$scope.t.tournaments.forEach(function(tny){
+				if (tny.tournament.url === $scope.tournamentId) {
+					$scope.activeTournament = tny;
+					view();
+					$scope.found = true;
+				}
+			});
 		});
 
 	});
