650ea32b1da6cc4ae76b0fd82cac02c1df966664
Aaron Tidwell <Aaron.Tidwell@gmail.com>
2014-01-02 03:52:42 -0500

    nav

--- public/app/scripts/controllers/nav.js
+++ public/app/scripts/controllers/nav.js
@@ -1,10 +1,17 @@
 'use strict';
 
 angular.module('hearthApp')
-	.controller('NavCtrl', function($scope, user) {
+	.controller('NavCtrl', function($scope, user, activeTournament, $location) {
 		$scope.user = user.get();
+
+		$scope.at = activeTournament.get();
 
 		$scope.login = user.login;
 		$scope.countUsers = user.countUsers;
 
+		$scope.navClass = function(page) {
+			var currentRoute = $location.path().substring(1).split('/')[0] || 'tournaments';
+			console.log(page,currentRoute)
+			return page === currentRoute ? 'active' : '';
+		};
 	});
