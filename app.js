e250eb5cd3c94576ab83e44d66f48eb7d0084ca0
Aaron Tidwell <Aaron.Tidwell@gmail.com>
2014-01-04 21:29:59 -0500

    -n add build

---
+++ app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var server = require('./server/app');
+
+var env = require('./server/config/local');
+
+//process cmd-line args for environment
+var args = process.argv.splice(2);
+args.forEach(function(arg) {
+	var parts = arg.split('=');
+	switch (parts[0]) {
+		case '-e':
+		case '-env':
+			env = require('./server/config/' + parts[1]);
+			break;
+	}
+});
+
+//sync to read the modules from the filesystem
+var s = server.createServer(env);
+
+s.events.on('server:ready', function() {
+	s.modules.server.log('Server is ready');
+	s.modules.server.log(s);
+});
---
+++ server/app/modules/angularServer.js
@@ -0,0 +1,69 @@
+/*
+	Depends on: Module, Server
+
+	Subscribes:
+		server:routes - adds angular routes to the server
+*/
+var util = require('util');
+var path = require('path');
+var fs = require('fs');
+var express = require('express');
+
+var Module = require('./core/module').Module;
+
+var AngularServer = exports.AngularServer = function(options, events) {
+	//call parent constructor
+	Module.call(this, options);
+	var self = this;
+
+	var serverOptions = self.options.get('angularServer');
+
+	//generate the absolute path from the config
+	serverOptions.staticPath = self.generateStaticDirectory(serverOptions.staticDirectory);
+
+	//add the static frontend server
+    events.on('server:configure', function(app){
+        app.use(express.static(serverOptions.staticPath));
+    });
+	
+	//add the routes
+	events.on('server:routes', function(app){
+		app.get(serverOptions.uriPath + '*', function(req,res) { //serve all /uriPath/* routes
+			self.serve(req,res);
+		});
+	});
+};
+
+util.inherits(AngularServer, Module);
+
+/*
+	Serves the angular app
+
+	If its just the uriPath route, redirect to index
+
+	If the file exists, serve it, otherwise serve the index.html
+		DANGER -- assumes single-point of entry and no 404
+*/
+AngularServer.prototype.serve = function(req,res) {
+	var self = this;
+	var serverOptions = this.options.get('angularServer');
+
+	//strip off the leading /uriPath portion of the url
+	var path = req.path;
+
+	//we have a real path - check if the file exists
+	var file = serverOptions.staticPath + path;
+	fs.exists(file, function(exists) {
+		if (exists) {
+			//serve it
+			res.sendfile(path, {
+				root: serverOptions.staticPath
+			});
+		} else {
+			//otherwise serve the index
+			res.sendfile('index.html', {
+				root: serverOptions.staticPath
+			});
+		}
+	});
+};
