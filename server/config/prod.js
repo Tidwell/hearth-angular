f2fdf5865c1ce2c67c44eb60e73db9db8750c948
Aaron Tidwell <Aaron.Tidwell@gmail.com>
2014-01-18 22:22:15 -0500

    -n fixing prod config

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,23 +1,23 @@
 module.exports = {
-	env: 'prod',
-	server: {
-		port: 8080
-	},
-	angularServer: {
-		staticDirectory: '/public',
-		uriPath: '/'
-	},
-	socket: {
-		log: false
-	},
-	tournaments: {
-		key: 'S6e1RskMPjjQyEYFXmGu1IZ178hISWxtlVNC3yQn',
-		subdomain: 'hs_tourney',
-		regions: ['NA', 'EU'],
-		regionPrefix: '[',
-		regionSuffix: ']',
-		namePrefix: '8 Player Tournament ',
-		dropTimeout: 240000
-	},
-	db: 'mongodb://localhost/hearth-angular'
+        env: 'prod',
+        server: {
+                port: 9007
+        },
+        angularServer: {
+                staticDirectory: '/public',
+                uriPath: '/'
+        },
+        socket: {
+                log: false
+        },
+        tournaments: {
+                key: 'S6e1RskMPjjQyEYFXmGu1IZ178hISWxtlVNC3yQn',
+                subdomain: 'hs_tourney',
+                regions: ['NA', 'EU'],
+                regionPrefix: '[',
+                regionSuffix: ']',
+                namePrefix: '8 Player Tournament ',
+                dropTimeout: 240000
+        },
+        db: 'mongodb://localhost/hearth-angular'
 };
