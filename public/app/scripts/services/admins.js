5399bc689461c5af70def5283f33734a7c8864fd
Aaron Tidwell <Aaron.Tidwell@gmail.com>
2014-02-15 10:26:28 -0500

    adding  Noah#1105 as admin

--- public/app/scripts/services/admins.js
+++ public/app/scripts/services/admins.js
@@ -2,7 +2,7 @@
 
 angular.module('hearthApp')
 	.service('admins', function Admins() {
-		var adminList = ['Tidwell#1482', 'zzyx#1198', 'Zzyx#1198'];
+		var adminList = ['Tidwell#1482', 'zzyx#1198', 'Zzyx#1198', 'Noah#1105', 'noah#1105'];
 
 		return {
 			get: function() {
