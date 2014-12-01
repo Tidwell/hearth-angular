17f17c90f11b3d5435667da08de401a40c1a4464
Aaron Tidwell <Aaron.Tidwell@gmail.com>
2014-12-01 03:44:49 -0500

    adding 503

---
+++ server/config/local.js
@@ -0,0 +1,24 @@
+module.exports = {
+        env: 'local',
+        server: {
+                port: 8080
+        },
+        angularServer: {
+                staticDirectory: '/public/app',
+                uriPath: '/'
+        },
+        socket: {
+                log: false
+        },
+        tournaments: {
+                key: 'S6e1RskMPjjQyEYFXmGu1IZ178hISWxtlVNC3yQn',
+                subdomain: 'nodeapitest',
+                regions: ['NA', 'EU'],
+                regionPrefix: '[',
+                regionSuffix: ']',
+                namePrefix: '8 Player Tournament ',
+                dropTimeout: 60000
+        },
+        db: 'mongodb://localhost/hearth-angular'
+};
+
---
+++ server/config/stage.js
@@ -0,0 +1,33 @@
+
+module.exports = {
+        env: 'stage',
+        server: {
+                port: 8080
+        },
+        angularServer: {
+                staticDirectory: '/public',
+                uriPath: '/'
+        },
+        socket: {
+                log: false
+        },
+        tournaments: {
+                key: 'S6e1RskMPjjQyEYFXmGu1IZ178hISWxtlVNC3yQn',
+                subdomain: 'nodeapitest',
+                regions: ['NA', 'EU'],
+                regionPrefix: '[',
+                regionSuffix: ']',
+                namePrefix: '8 Player Tournament ',
+                dropTimeout: 60000
+        },
+        db: 'mongodb://localhost/hearth-angular'
+};
+
+
+
+
+
+
+
+
+
